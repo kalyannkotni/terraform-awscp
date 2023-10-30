f0aa77ca23bd7f3d587863727242026c49acbac8
kalyannkotni <127472317+kalyannkotni@users.noreply.github.com>
2023-10-30 21:37:08 +0530

    Update provider.tf

--- terraform-manifests/provider.tf
+++ terraform-manifests/provider.tf
@@ -1,8 +1,10 @@
 # Terraform Block
 terraform {
+  required_version = "~> 0.14"
   required_providers {
     aws = { 
       source = "hashicorp/aws"
+      version = "~> 3.0"
     }
   }
   backend "s3" {
