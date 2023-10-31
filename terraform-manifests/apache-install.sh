bf5fb4201d5646a2b05f8ae6cf385620f955e870
kalyannkotni <127472317+kalyannkotni@users.noreply.github.com>
2023-10-31 12:04:27 +0530

    Delete terraform-manifests directory

--- terraform-manifests/apache-install.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install -y httpd
-sudo service httpd start  
-sudo systemctl enable httpd
-echo "succesfully completed assignment-5.2 VM" > /var/www/html/index.html
